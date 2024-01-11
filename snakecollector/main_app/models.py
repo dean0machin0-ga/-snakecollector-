a4303e3696772b639c717c8a3588e1d37559c4cc
dean0machin0 <dean0machin00@gmail.com>
2024-01-11 13:58:55 -0800

    Snake model created and migrated to the database.

--- snakecollector/main_app/models.py
+++ snakecollector/main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Snake(models.Model):
+    name = models.CharField(max_length=100)
+    native_to = models.TextField(max_length=300)
+    natural_habitat = models.TextField(max_length=300)
+    venom_level = models.CharField(max_length=100)
+    endangered = models.BooleanField()
