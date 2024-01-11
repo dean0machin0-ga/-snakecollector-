b0f35b57b1856263353ae828f6278923a6bee95d
dean0machin0 <dean0machin00@gmail.com>
2024-01-10 19:59:22 -0800

    Snake data added to views.py.

--- snakecollector/main_app/views.py
+++ snakecollector/main_app/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
 
+snakes = [
+    {
+        'name': 'Black Mamba',
+        'domain': 'Eukaryota',
+        'kingdom': 'Animalia',
+        'Phylum': 'Chordata',
+        'Class': 'Reptilia',
+        'Order': 'Squamata',
+        'Suborder':	'Serpentes',
+        'family': 'Elapidae',
+        'Genus': 'Dendroaspis',
+        'Species': 'D. polylepis',
+        'venom level': 'Highly Venomous',
+        'native to': 'sub-Saharan Africa',
+        'natural habitat': 'savannah, woodland'
+    },
+
+    {
+        'name': 'Inland Taipan',
+        'domain': 'Eukaryota',
+        'kingdom': 'Animalia',
+        'Phylum': 'Chordata',
+        'Class': 'Reptilia',
+        'Order': 'Squamata',
+        'Suborder':	'Serpentes',
+        'family': 'Elapidae',
+        'Genus': 'Oxyuranus',
+        'Species': 'O. microlepidotus',
+        'venom level': 'Most Venomous Snake in the World',
+        'native to': 'Goyder Lagoon in north-east South Australia',
+        'natural habitat': 'The black soil plains in the semiarid regions where the Queensland and South Australia borders converge'
+    },
+
+    {
+        'name': 'Echis (common names: saw-scaled vipers, carpet vipers)',
+        'domain': 'Eukaryota',
+        'kingdom': 'Animalia',
+        'Phylum': 'Chordata',
+        'Class': 'Reptilia',
+        'Order': 'Squamata',
+        'Suborder':	'Serpentes',
+        'family': 'Viperidae',
+        'subfamily': 'Viperinae',
+        'venom level': 'Highly Venomous',
+        'native to': 'Pakistan, India (in rocky regions of Maharashtra, Rajasthan, Uttar Pradesh, and Punjab) and Sri Lanka, parts of the Middle East, and Africa north of the equator.',
+        'natural habitat': 'sand, rock, soft soil and in scrublands. It is often found hiding under loose rocks'
+    }
+]
+
 # Create your views here.
