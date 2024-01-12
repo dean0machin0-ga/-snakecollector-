5e6b9b4f7733fbb95d6ec7f34089e348008cc032
dean0machin0 <dean0machin00@gmail.com>
2024-01-11 21:54:01 -0800

    Updated Snake model through python shell with Diet attribute and rendered in details.

--- snakecollector/main_app/models.py
+++ snakecollector/main_app/models.py
@@ -5,6 +5,7 @@ class Snake(models.Model):
     name = models.CharField(max_length=100)
     native_to = models.TextField(max_length=300)
     natural_habitat = models.TextField(max_length=300)
+    diet = models.TextField(max_length=125, default='Food')
     venom_level = models.CharField(max_length=100)
     endangered = models.BooleanField()
 
